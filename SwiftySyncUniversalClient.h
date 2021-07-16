b75c03aea9727ab3476154d218dd9ea38250f5ec
Yauheni Stsefankou <stefjen@mail.ru>
2021-07-17 01:23:32 +0300

    Libraries init

--- SwiftySyncUniversalClient.h
+++ SwiftySyncUniversalClient.h
@@ -1,8 +1,28 @@
 #ifndef SWIFTY_SYNC_UNIVERSAL_CLIENT
 #define SWIFTY_SYNC_UNIVERSAL_CLIENT
 
-class SwiftyUniversalClient {
+#include <websocketpp/config/asio_no_tls_client.hpp>
+#include <websocketpp/client.hpp>
+#include "Codable.h"
+#include "JSON.h"
+#include <iostream>
 
+typedef websocketpp::client<websocketpp::config::asio_client> client;
+
+using namespace std;
+
+#define PRINT_LOG [](const std::string& strLogMsg) { std::cout << strLogMsg << std::endl;  }
+
+class SwiftyUniversalClient {
+	client cli;
+
+	void run() {
+		
+	}
+
+	SwiftyUniversalClient() {}
+
+	~SwiftyUniversalClient() {}
 };
 
 #endif
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "SwiftySyncUniversalClient.h"
+
+using namespace std;
+
+int main() {
+	return 0;
+}
