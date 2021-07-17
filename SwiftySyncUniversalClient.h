d815e31e90ef882a15071323b1b4f3678e919214
Yauheni Stsefankou <stefjen@mail.ru>
2021-07-17 21:22:50 +0300

    Connection sample (echo) implemented

--- SwiftySyncUniversalClient.h
+++ SwiftySyncUniversalClient.h
@@ -9,18 +9,52 @@
 
 typedef websocketpp::client<websocketpp::config::asio_client> client;
 
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
 using namespace std;
 
-#define PRINT_LOG [](const std::string& strLogMsg) { std::cout << strLogMsg << std::endl;  }
+typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
+
+namespace {
+	void on_message(client* c, websocketpp::connection_hdl hdl, message_ptr msg) {
+		websocketpp::lib::error_code ec;
+
+		c->send(hdl, msg->get_payload(), msg->get_opcode(), ec);
+		if (ec) {
+			cout << "Message send failed: " << ec.message() << "\n";
+		}
+	}
+}
 
 class SwiftyUniversalClient {
-	client cli;
+public:
+	client c;
 
-	void run() {
-		
+	SwiftyUniversalClient(string uri) {
+		try {
+			c.set_access_channels(websocketpp::log::alevel::all);
+			c.clear_access_channels(websocketpp::log::alevel::frame_payload);
+
+			c.init_asio();
+
+			c.set_message_handler(bind(&on_message, &c, ::_1, ::_2));
+
+			websocketpp::lib::error_code ec;
+			client::connection_ptr con = c.get_connection(uri, ec);
+			if (ec) {
+				cout << "Could not create connection: " << ec.message() << "\n";
+				return;
+			}
+
+			c.connect(con);
+
+			c.run();
+		}
+		catch (websocketpp::exception const& e) {
+			cout << e.what() << "\n";
+		}
 	}
-
-	SwiftyUniversalClient() {}
 
 	~SwiftyUniversalClient() {}
 };
--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 using namespace std;
 
 int main() {
+	SwiftyUniversalClient client("ws://localhost:8888");
 	return 0;
 }
