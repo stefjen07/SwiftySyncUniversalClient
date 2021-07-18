6908b4399842d2b44f3b0ad36ce6908e545778e5
Yauheni Stsefankou <stefjen@mail.ru>
2021-07-18 18:43:48 +0300

    Authorization request

--- SwiftySyncUniversalClient.h
+++ SwiftySyncUniversalClient.h
@@ -3,8 +3,16 @@
 
 #include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
-#include "Codable.h"
-#include "JSON.h"
+
+#include <websocketpp/common/thread.hpp>
+#include <websocketpp/common/memory.hpp>
+
+#include <Authorization.h>
+#include <SwiftySyncStorage.h>
+#include <Codable.h>
+#include <JSON.h>
+#include <functional>
+#include <vector>
 #include <iostream>
 
 typedef websocketpp::client<websocketpp::config::asio_client> client;
@@ -16,32 +24,62 @@ using namespace std;
 
 typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
 
-namespace {
-	void on_message(client* c, websocketpp::connection_hdl hdl, message_ptr msg) {
-		websocketpp::lib::error_code ec;
-
-		c->send(hdl, msg->get_payload(), msg->get_opcode(), ec);
-		if (ec) {
-			cout << "Message send failed: " << ec.message() << "\n";
-		}
-	}
-}
-
 class SwiftyUniversalClient {
 public:
 	client c;
+	client::connection_ptr con;
+	websocketpp::lib::shared_ptr<websocketpp::lib::thread> thread;
 
-	SwiftyUniversalClient(string uri) {
+	vector<AuthorizationProvider*> supportedProviders;
+	string uri;
+	function<void(AuthorizationStatus)> authHandler;
+
+	void on_message(websocketpp::connection_hdl hdl, message_ptr msg) {
+		string respond = msg->get_payload();
+
+		if (respond.find(AUTH_PREFIX) == 0) {
+			handleAuthRespond(respond.substr(strlen(AUTH_PREFIX), respond.length() - strlen(AUTH_PREFIX)));
+		}
+	}
+
+	void authorize(int providerIndex, string credentials) {
+		string request = AUTH_PREFIX;
+		auto provider = supportedProviders[providerIndex];
+		request += provider->generateRequest(credentials);
+		websocketpp::lib::error_code ec;
+		c.send(con->get_handle(), &request, request.length(), websocketpp::frame::opcode::text, ec);
+		if (ec) {
+			cout << ec.message();
+		}
+	}
+
+	void handleAuthRespond(string body) {
+		AuthorizationStatus status;
+		if (body == AUTHORIZED_LOCALIZE) {
+			status = AuthorizationStatus::authorized;
+		}
+		else if (body == CORR_CRED_LOCALIZE) {
+			status = AuthorizationStatus::corruptedCredentials;
+		}
+		else if (body == AUTH_ERR_LOCALIZE) {
+			status = AuthorizationStatus::error;
+		}
+		authHandler(status);
+	}
+
+	void run() {
 		try {
 			c.set_access_channels(websocketpp::log::alevel::all);
 			c.clear_access_channels(websocketpp::log::alevel::frame_payload);
 
 			c.init_asio();
 
-			c.set_message_handler(bind(&on_message, &c, ::_1, ::_2));
+			c.set_message_handler([this](websocketpp::connection_hdl hdl, message_ptr msg) {
+				on_message(hdl, msg);
+			});
 
 			websocketpp::lib::error_code ec;
-			client::connection_ptr con = c.get_connection(uri, ec);
+			con = c.get_connection(uri, ec);
 			if (ec) {
 				cout << "Could not create connection: " << ec.message() << "\n";
 				return;
@@ -49,14 +87,22 @@ public:
 
 			c.connect(con);
 
-			c.run();
+			c.start_perpetual();
+
+			thread.reset(new websocketpp::lib::thread(&client::run, &c));
 		}
 		catch (websocketpp::exception const& e) {
 			cout << e.what() << "\n";
 		}
 	}
 
-	~SwiftyUniversalClient() {}
+	SwiftyUniversalClient(string uri) {
+		this->uri = uri;
+	}
+
+	~SwiftyUniversalClient() {
+		c.stop_perpetual();
+	}
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
-#include "SwiftySyncUniversalClient.h"
+#include <GoogleAuthorization.h>
+#include <FacebookAuthorization.h>
+#include <Authorization.h>
+#include <SwiftySyncUniversalClient.h>
 
 using namespace std;
 
 int main() {
 	SwiftyUniversalClient client("ws://localhost:8888");
+	
+	auto facebookProvider = FacebookProvider();
+	auto castedFacebookProvider = dynamic_cast<AuthorizationProvider*>(&facebookProvider);
+	
+	auto googleProvider = GoogleProvider();
+	auto castedGoogleProvider = dynamic_cast<AuthorizationProvider*>(&googleProvider);
+	
+	client.supportedProviders = {
+		castedFacebookProvider,
+		castedGoogleProvider
+	};
+
+	client.authHandler = [](AuthorizationStatus status) {
+		if (status == AuthorizationStatus::authorized) {
+			cout << "Authorized successfully\n";
+		}
+	};
+
+	client.run();
+
+	client.authorize(0, "");
+
+	while (true);
 	return 0;
 }
